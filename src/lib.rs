34bcabb3478f8ff4aae3885a6843aa94f4956e8b
Maximilian von Gaisberg <github@vongaisberg.de>
2019-06-26 16:02:53 +0200

    Make code slightly more beautiful

--- src/lib.rs
+++ src/lib.rs
@@ -70,70 +70,29 @@ impl Universe {
         //let mut rng = rand::thread_rng();
 
         let mut rng = OsRng::new().unwrap();;
-        let mut elems = vec![PhysicsObject::<f64>::new(
-            [200f64, 400f64],
-            [0f64, 0f64],
-            150f64,
-        )];
-        elems.pop();
 
-        // let mut elems: Vec<PhysicsObject::<f64>>::new();
-        // for i in 0..60 {
-        //     for j in 0..60 {
-        //         //seed = (seed * 0x5DEECE66Du64 + 0xBu64) & ((1u64 << 48) - 1);
 
-        //         elems.push(PhysicsObject::<f64>::new(
-        //             [(40.0 * i as f64) + 400.0, (40.0 * j as f64) + 400.0],
-        //             [0f64, 0f64],
-        //             150f64,
-        //         ));
-        //     }
-        // }
-        // elems.push(PhysicsObject::<f64>::new(
-        //     [2800f64, 2800f64],
-        //     [-2f64, -0f64],
-        //     30000f64,
-        // ));
+        let mut elems = Vec::new();
+
+    let speed_range = 1.0;
 
         for i in 0..100 {
             elems.push(PhysicsObject::<f64>::new(
-                [rng.gen_range(200.0, 1000.0), rng.gen_range(200.0, 900.0)],
-                [rng.gen_range(-4.0, 4.0), rng.gen_range(-4.0, 4.0)],
-                rng.gen_range(100.0, 200.0),
+                [rng.gen_range(50.0, 800.0), rng.gen_range(50.0, 800.0)],
+                [rng.gen_range(-speed_range, speed_range), rng.gen_range(-speed_range, speed_range)],
+                rng.gen_range(1.0, 2.0),
             ))
         }
-
-        // let mut elems = vec![
-        //     PhysicsObject::<f64>::new([200f64, 400f64], [0f64, 0f64], 150f64),
-        //     PhysicsObject::<f64>::new([200f64, 200f64], [0f64, 0f64], 150f64),
-        //     PhysicsObject::<f64>::new([200f64, 109f64], [0f64, 0f64], 150f64),
-        //     PhysicsObject::<f64>::new([250f64, 100f64], [0f64, 0f64], 150f64),
-        //     PhysicsObject::<f64>::new([300f64, 240f64], [0f64, 0f64], 150f64),
-        //     PhysicsObject::<f64>::new([270f64, 140f64], [0f64, 0f64], 150f64),
-        //     PhysicsObject::<f64>::new([400f64, 40f64], [0f64, 0f64], 150f64),
-        //     PhysicsObject::<f64>::new([350f64, 120f64], [0f64, 0f64], 150f64),
-        //     PhysicsObject::<f64>::new([400f64, 400f64], [0f64, 0f64], 150f64),
-        // ];
-
-        // for i in 1..20 {
-        //     elems.push(PhysicsObject::<f64>::new(
-        //         [rng.gen_range(100.0, 300.0), rng.gen_range(100.0, 300.0)],
-        //         // [rng.gen_range(-0.002, 0.002), rng.gen_range(-0.02, 0.02)],
-        //         [0., 0.],
-        //         //rng.gen_range(50.0, 60.0),
-        //         1.0,
-        //     ))
-        // }
 
         Universe {
             phys: PhysicsSpace::new(
                 elems,
-                0.3f64,
+                10f64,
                 EuclideanSpace::<f64> {
                     field: std::marker::PhantomData::<f64>,
                 },
-                30000f64,
-                15f64,
+                3000f64,
+                5f64,
             ),
         }
     }
